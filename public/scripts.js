17b46f254b98ed5556ceba325d997d9ec367a3bf
Tory Dannenberg <rfdberg@gmail.com>
2018-09-04 09:50:59 -0600

    Add html for cards section and add initial functionality to append new items to page

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,32 @@
 console.log('hello world')
+
+
+const handleSubmit = () => {
+  let title = $('.title-input').val();
+  let description = $('.description-input').val();
+  console.log(title, description)
+  appendListItem(title, description);
+  clearInputs();
+}
+
+const appendListItem = (title, description) => {
+  $('.bucket-list-container').append(`
+    <div class="list-item-card">
+      <h3 class="item-title-headers">${title}</h3>
+      <p class="item-description-paragraphs"> ${description} </p>
+      <button class="delete-buttons">Delete</button>      
+    </div>
+  `)
+}
+
+const saveItemToDatabase = (title, description) => {
+  console.log('send to database');
+}
+
+const clearInputs = () => {
+  $('.title-input').val('');
+  $('.description-input').val('');
+}
+
+$('.submit-button').on('click', handleSubmit);
+
