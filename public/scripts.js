c1b46af4cf1dea292f848a4d0c76e62ac35151f2
Tory Dannenberg <rfdberg@gmail.com>
2018-09-04 10:39:20 -0600

    Add fetch call to save new items to DB

--- public/scripts.js
+++ public/scripts.js
@@ -1,26 +1,32 @@
-console.log('hello world')
 
-
-const handleSubmit = () => {
+const handleSubmit = async () => {
   let title = $('.title-input').val();
   let description = $('.description-input').val();
-  console.log(title, description)
-  appendListItem(title, description);
+  const response = await saveItemToDatabase(title, description);
+  appendListItem(title, description, response.id);
   clearInputs();
 };
 
-const appendListItem = (title, description) => {
+const appendListItem = (title, description, id) => {
   $('.bucket-list-container').append(`
     <div class="list-item-card">
       <h3 class="item-title-headers">${title}</h3>
       <p class="item-description-paragraphs"> ${description} </p>
-      <button class="delete-buttons">Delete</button>      
+      <button class="delete-buttons ${id}">Delete</button>      
     </div>
   `)
 };
 
-const saveItemToDatabase = (title, description) => {
-  console.log('send to database');
+const saveItemToDatabase = async (title, description) => {
+  const results = await fetch('/api/v1/bucketlist', {
+    method: 'POST',
+    body: JSON.stringify({ title, description }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  const response = await results.json();
+  return response;
 };
 
 const clearInputs = () => {
