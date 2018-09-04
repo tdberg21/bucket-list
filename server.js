60723dbf40fd4ec93021a34e08bfb8bbd8c97639
Tory Dannenberg <rfdberg@gmail.com>
2018-09-04 09:34:40 -0600

    Run initial migration and set up schema, update get route handler

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Bucket List';
@@ -10,9 +13,13 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/v1/bucketlist', (request, response) => {
-  const { title, description } = request.body;
-
-  response.json({ title, description });
+  database('listitems').select()
+    .then((listitems) => {
+      response.status(200).json(listitems);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.listen(3000, () => {
