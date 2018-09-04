9a0781a9fbf7c79aeb54fe29cc03c02bcf551de3
Tory Dannenberg <rfdberg@gmail.com>
2018-09-04 12:22:03 -0600

    Add happy and sad tests for post

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -65,6 +65,47 @@ describe('API Routes', () => {
           response.should.have.status(200);
           response.should.be.json;
           response.body.should.be.a('array');
+          response.body.should.have.length(3);
+          response.body[0].should.have.property('id');
+          response.body[0].should.have.property('title');
+          response.body[0].should.have.property('description');
+          response.body[0].id.should.equal(1);
+          response.body[0].title.should.equal('listItem1');
+          response.body[0].description.should.equal('item number 1')
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/v1/bucketlist', () => {
+    it('should add a bucket list item', done => {
+      chai.request(app)
+        .post('/api/v1/bucketlist')
+        .send({
+          "title": "New Item Title",
+          "description": "New Item description"
+        })
+        .end((error, response) => {
+          // response.should.have.status(200);
+          response.should.be.json;
+          // response.body.should.be.a('object');
+          // response.body.should.have.property('id');
+          // response.body.id.should.equal(4);
+          done();
+        });
+    });
+
+    it('should not create a list item with missing parameters', done => {
+      chai.request(app)
+        .post('/api/v1/bucketlist')
+        .send({
+          title: 'Testing'
+        })
+        .end((error, response) => {
+          response.should.have.status(422);
+          response.should.be.json;
+          response.body.should.be.a('object');
+          response.body.should.have.property('error');
           done();
         });
     });
